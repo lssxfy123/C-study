0fef491bceb482eb3571a2a74128cca2a0b79041
lssxfy123 <liushenshenxfy@126.com>
2015-08-29 17:29:27 +0800

    函数模板的学习

---
+++ template_test1/template_test1/test.cpp
@@ -0,0 +1,35 @@
+// Copyright 2015.Áõ«|«|
+// author£ºÁõ«|«|
+// º¯ÊýÄ£°åµÄ²âÊÔ
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+template<class T>
+const T& FindMax(const vector<T>& a)
+{
+    int max_index = 0;
+    for (int i = 0; i != a.size(); ++i)
+    {
+        if (a[max_index] < a[i])
+        {
+            max_index = i;
+        }
+    }
+
+    return a[max_index];
+}
+
+int main(int argc, char* argv[])
+{
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+
+    cout << FindMax(a) << endl;
+    return 0;
+}
+
