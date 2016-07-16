b0a1b2ebd341c2ea14cbea6ee862ef9be4681af3
lssxfy123 <liushenshenxfy@126.com>
2016-07-16 16:21:59 +0800

    增加之前的算法学习文档，二叉查找树

--- binary_search_tree_test1/test.cpp
+++ binary_search_tree_test1/test.cpp
@@ -1,72 +1,127 @@
 // Copyright 2016.刘珅珅
 // author：刘珅珅
-// 二叉树：利用后缀表达式生成，如果是中缀表达式，则先转换为后缀表达式
+// 二叉查找树
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <stack>
 #include <string>
 using namespace std;
 
-template <typename Comparable>
+template <typename Object, typename Comparator = less<Object> >
 class BinarySearchTree
 {
 public:
-    BinarySearchTree();
-    BinarySearchTree(const BinarySearchTree& rhs);
-    ~BinarySearchTree();
+    BinarySearchTree()
+    {
+        root_ = NULL;
+    }
 
-    const Comparable& FindMin() const;
-    const Comparable& FindMax() const;
-    bool Contains(const Comparable& x) const;
+    BinarySearchTree(const BinarySearchTree& rhs);
+    ~BinarySearchTree()
+    {
+    }
+
+    const Object& FindMin() const;
+    const Object& FindMax() const;
+    bool Contains(const Object& x) const;
     bool IsEmpty() const;
-    void PrintTree() const;
+    void PrintTree() const
+    {
+        PrintTree(root_);
+    }
 
     void MakeEmpty();
-    void Insert(const Comparable& x);
-    void Remove(const Comparable& x);
+    void Insert(const Object& x)
+    {
+        Insert(x, root_);
+    }
+
+    void Remove(const Object& x);
 
     const BinarySearchTree& operator= (const BinarySearchTree& rhs);
 
 private:
     struct BinaryNode
     {
-        Comparable element_;
+        Object element_;
         BinaryNode* left_;
         BinaryNode* right_;
 
-        BinaryNode(const Comparable& element, BinaryNode* lt, BinaryNode* rt)
-        : element_(element), left_(lt), right(rt) {}
+        BinaryNode(const Object& element, BinaryNode* lt, BinaryNode* rt)
+        : element_(element), left_(lt), right_(rt) {}
     };
 
-    BinaryNode* root_;
-    void Insert(const Comparable& x, BinaryNode*& t) const;
-    void Remove(const Comparable& x, BinaryNode*& t) const;
+    // 使用了指针的引用
+    // 在递归调用过程中，修改t的指向会改变外层的t的指向，从而形成树
+    void Insert(const Object& x, BinaryNode*& t) const
+    {
+        if (t == NULL)
+        {
+            t = new BinaryNode(x, NULL, NULL);
+        } else if (IsLessThan(x, t->element_))
+        {
+            Insert(x, t->left_);
+        } else if (IsLessThan(t->element_, x))
+        {
+            Insert(x, t->right_);
+        }
+
+        // 可以看出这个插入函数没法插入重复元
+    }
+
+    void Remove(const Object& x, BinaryNode*& t) const;
     BinaryNode* FindMin(BinaryNode* t) const;
     BinaryNode* FindMax(BinaryNode* t) const;
-    bool Contains(const Comparable& x, BinaryNode* t) const;
+    bool Contains(const Object& x, BinaryNode* t) const
+    {
+        if (t == NULL)
+        {
+            return false;
+        } else if (IsLessThan(x, t->element_)) {
+            return Contains(x, t->left_);
+        } else if (IsLessThan(t->element_, x)) {
+            return Contains(x, t->right_);
+        } else {
+            return true;
+        }
+    }
+
     void MakeEmpty(BinaryNode*& t);
-    void PrintTree(BinaryNode* t) const;
+    void PrintTree(BinaryNode* t) const
+    {
+        if (t == NULL)
+        {
+            return;
+        }
+
+        PrintTree(t->left_);
+        cout << t->element_ << ' ';
+        PrintTree(t->right_);
+    }
+
     BinaryNode* Clone(BinaryNode* t) const;
+
+private:
+    BinaryNode* root_;
+    Comparator IsLessThan;
 };
+
+template<typename Object, typename Comparator> 
+bool BinarySearchTree<Object, Comparator>:: Contains(const Object& x) const
+{
+    return Contains(x, root_);
+}
 
 int main(int argc, char* argv[])
 {
-    // 中缀表达式(标准表达式)
-    string expression = "(a+(b*c))+(((d*e)+f)*g)";
-    string last = MiddleToLast(expression);
-    cout << "last: " << last << endl;
-    BinaryNode* root = CreateBinaryTree(last);
-
-    cout << "后序遍历：" << endl;
-    PostOrderTraverse(root);
-    cout << endl;
-
-    cout << "先序遍历：" << endl;
-    PreOrderTraverse(root);
-    cout << endl;
-
-    cout << "中序遍历：" << endl;
-    InOrderTraverse(root);
-
+    BinarySearchTree<float> binary_search_tree;
+    binary_search_tree.Insert(3.2);
+    binary_search_tree.Insert(3.5);
+    binary_search_tree.Insert(4.6);
+    binary_search_tree.Insert(2.9);
+    binary_search_tree.Insert(5.4);
+    binary_search_tree.PrintTree();
     cout << endl;
     return 0;
 }
