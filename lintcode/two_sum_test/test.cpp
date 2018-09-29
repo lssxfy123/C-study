250aa00bca6e71f38c4f2d3652730600354cd400
lssxfy123 <liushenshenxfy@126.com>
2018-09-29 15:58:34 +0800

    lintcode刷题：两数之和

--- lintcode/two_sum_test/test.cpp
+++ lintcode/two_sum_test/test.cpp
@@ -1,7 +1,7 @@
 // Copyright 2018.刘珅珅
 // author：刘珅珅
 // 两数之和
-#include <algorithm>
+#include <unordered_map>
 #include <iostream>
 #include <vector>
 #include "../../include/print.hpp"
@@ -14,13 +14,34 @@ vector<int> twoSum(vector<int> &numbers, int target) {
 	{
 		return result;
 	}
-	sort(numbers.begin(), numbers.end());
+
+	unordered_map<int, int> hash;
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		if (hash.find(target - numbers[i]) != hash.end())
+		{
+			// target - numbers[i]下标更小
+			result.push_back(hash[target - numbers[i]]);
+			result.push_back(i);
+			return result;
+		}
+		hash[numbers[i]] = i;
+	}
+	result.push_back(-1);
+	result.push_back(-1);
 	return result;
 }
 
 int main(int argc, char* argv[])
 {
 	vector<int> numbers{ 7, 2, 13, 9 };
-	twoSum(numbers, 9);
+	vector<int> result;
+	result = twoSum(numbers, 9);
+	for (int i = 0; i < result.size(); ++i)
+	{
+		cout << result[i] << ",";
+	}
+	cout << endl;
+	system("pause");
 	return 0;
 }
