6d90e13117987560d3999792a11b7b8bf01d569c
lssxfy123 <liushenshenxfy@126.com>
2018-09-30 17:49:30 +0800

    lintcode 字符串比较

--- lintcode/compare_string_test/test.cpp
+++ lintcode/compare_string_test/test.cpp
@@ -3,7 +3,7 @@
 // 字符串比较
 // B中所有字符都要在A中且每个字符个数<=A中对应字符个数
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include "../../include/print.hpp"
 using namespace std;
 
@@ -13,7 +13,14 @@ bool compareStrings(string &A, string &B) {
     {
         return false;
     }
-    map<char, int> map_B;
+
+    if (B.length() == 0)
+    {
+        return true;
+    }
+
+    unordered_map<char, int> map_B;
+    unordered_map<char, int> map_A;
     for (int i = 0; i < B.length(); ++i)
     {
         if (map_B.find(B[i]) != map_B.end())
@@ -25,13 +32,39 @@ bool compareStrings(string &A, string &B) {
             map_B[B[i]] = 1;
         }
     }
-    return false;
+
+    for (int i = 0;i < A.length(); ++i)
+    {
+        if (map_A.find(A[i]) != map_A.end())
+        {
+            map_A[A[i]] = map_A[A[i]] + 1;
+        }
+        else
+        {
+            map_A[A[i]] = 1;
+        }
+    }
+
+    if (map_B.size() > map_A.size())
+    {
+        return false;
+    }
+
+    for (auto itor : map_B)
+    {
+        if (map_A.find(itor.first) == map_A.end() || map_A[itor.first] < map_B[itor.first])
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 int main(int argc, char* argv[])
 {
-    string A = "ABCD";
-    string B = "AABC";
+    string A = "ABC";
+    string B = "A";
     compareStrings(A, B);
     return 0;
 }
