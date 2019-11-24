9bf5e6fc907d5ebf2f83e647bb60dcad40d33f47
lssxfy123 <liushenshenxfy@126.com>
2019-11-24 21:35:30 +0800

    C++复习

--- lintcode/kth_largest_element_test/test.cpp
+++ lintcode/kth_largest_element_test/test.cpp
@@ -3,6 +3,8 @@
 // 第k大元素
 // 备注：时间复杂度O(n) 空间复杂度O(1)
 // 使用快速查找
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <vector>
 #include "../../include/print.hpp"
@@ -103,6 +105,63 @@ void QuickSelect(vector<int>& nums, int left, int right, int k)
 	}
 }
 
+int partition(vector<int>& array, int left, int right)
+{
+    int pivot = array[left];
+    int pivot_position = left;
+
+    for (int i = left + 1; i <= right; ++i)
+    {
+        if (array[i] > pivot)
+        {
+            ++pivot_position;
+            if (i != pivot_position)
+            {
+                swap(array[i], array[pivot_position]);
+            }
+        }
+    }
+
+    if (pivot_position != left)
+    {
+        swap(array[left], array[pivot_position]);
+    }
+
+    return pivot_position;
+}
+
+void QuickSelect1(vector<int>& nums, int left, int right, int k)
+{
+    if (left >= right)
+    {
+        return;
+    }
+
+    if (left + 10 <= right)
+    {
+        int pivot_position = partition(nums, left, right);
+        //if (k == pivot_position - 1)
+        //{
+        //    return;
+        //}
+
+        //if (k < pivot_position - 1)
+        {
+            QuickSelect1(nums, 0, pivot_position - 1, k);
+        }
+        //else
+        {
+            QuickSelect1(nums, pivot_position + 1, right, k);
+        }
+
+    }
+    else
+    {
+        InsertionSort(nums, left, right);
+    }
+
+}
+
 int kthLargestElement(int n, vector<int> &nums) {
 	// write your code here
 	if (nums.size() == 0 || n <= 0 || n > nums.size())
@@ -112,6 +171,17 @@ int kthLargestElement(int n, vector<int> &nums) {
 
 	QuickSelect(nums, 0, nums.size() - 1, n);
 	return nums[n - 1];
+}
+
+int kthLargestElement1(int n, vector<int> &nums) {
+    // write your code here
+    if (nums.size() == 0 || n <= 0 || n > nums.size())
+    {
+        return -1;
+    }
+
+    QuickSelect1(nums, 0, nums.size() - 1, n);
+    return nums[n - 1];
 }
 
 int main(int argc, char* argv[])
@@ -125,5 +195,10 @@ int main(int argc, char* argv[])
 	k = 4;
 	cout << kthLargestElement(k, vec1) << endl;
 	cout << kthLargestElement(k, vec2) << endl;
+
+    vector<int> vec3{ 595240,373125,463748,417209,209393,747977,864346,419023,925673,307640,597868,833339,130763,814627,766415,79576,459038,990103,944521,708820,473246,499960,742286,758503,270229,991199,770718,529265,498975,721068,727348,29619,712557,724373,823743,318203,290432,476213,412181,869308,496482,793858,676162,165869,160511,260864,502521,611678,786798,356560,916620,922168,89350,857183,964051,979979,916565,186532,905289,653307,351329,195491,866281,183964,650765,675046,661642,578936,78684,50105,688326,648786,645823,652329,961553,381367,506439,77735,707959,373271,316194,185079,686945,342608,980794,78777,687520,27772,711098,661265,167824,688245,286419,400823,198119,35400,916784,81169,874377,377128,922531,866135,319912,867697,10904 };
+    k = 100;
+    sort(vec3.begin(), vec3.end(), greater<int>());
+    cout << kthLargestElement1(k, vec3) << endl;
 	return 0;
 }
